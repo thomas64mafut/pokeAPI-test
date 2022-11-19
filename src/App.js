227f17c62ef4303005ae2e8d06210c5051a261d2
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2022-11-19 10:46:10 -0300

    fixed get evolution chain and show img (#2)
    * te odio api
    
    * fixed show evolution chain

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Home from './components/layout/home/Home';
 import Header from './components/layout/header/Header';
-import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
-      <Footer />
     </div>
   );
 }
